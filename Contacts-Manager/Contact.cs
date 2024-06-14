0fe8ee2c298afc98e4f4d90fe3e64e301af0bc39
RawanYaghmour <Rawan.yag02@gmail.com>
2024-06-14 22:23:47 +0300

    add lab 3

---
+++ Contacts-Manager/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Manager
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public Contact(string name, string category)
+        {
+            Name = name;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Category})";
+        }
+    }
+}
